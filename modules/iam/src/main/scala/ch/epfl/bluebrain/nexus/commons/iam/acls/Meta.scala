02df6cd5026233b2e4afc9a43fb65e41ee1a6ca8
Didac <didac.montero.mendez@gmail.com>
2017-10-30 14:23:23 +0100

    Moved Meta case class from IAM to commons (#24)

---
+++ modules/iam/src/main/scala/ch/epfl/bluebrain/nexus/commons/iam/acls/Meta.scala
@@ -0,0 +1,13 @@
+package ch.epfl.bluebrain.nexus.commons.iam.acls
+
+import java.time.Instant
+
+import ch.epfl.bluebrain.nexus.commons.iam.identity.Identity
+
+/**
+  * Type definition that aggregates ''author'' and ''timestamp'' as meta information for commands and events
+  *
+  * @param author  the origin of a Command or Event
+  * @param instant the moment in time when a Command or Event were emitted
+  */
+final case class Meta(author: Identity, instant: Instant)
