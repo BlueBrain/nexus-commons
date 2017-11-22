e24ffbf39e88b8f8697ac09a0388fb21aa40f290
Didac Montero <didac.monteromendez@epfl.ch>
2017-11-22 14:41:05 +0100

    Sorted JSON-LD response with the provided OrderedKeys
    Fixes https://github.com/BlueBrain/nexus/issues/26

--- modules/ld/sparql-client/src/main/scala/ch/epfl/bluebrain/nexus/commons/sparql/client/SparqlCirceSupport.scala
+++ modules/ld/sparql-client/src/main/scala/ch/epfl/bluebrain/nexus/commons/sparql/client/SparqlCirceSupport.scala
@@ -2,31 +2,17 @@ package ch.epfl.bluebrain.nexus.commons.sparql.client
 
 import java.io.ByteArrayInputStream
 
-import akka.http.scaladsl.marshalling.{Marshaller, ToEntityMarshaller}
-import akka.http.scaladsl.model.{ContentTypeRange, HttpEntity}
-import akka.http.scaladsl.model.MediaTypes.`application/json`
 import akka.http.scaladsl.unmarshalling.{FromEntityUnmarshaller, Unmarshaller}
-import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport
-import io.circe.{Json, Printer}
+import ch.epfl.bluebrain.nexus.commons.http.{JsonLdCirceSupport, RdfMediaTypes}
 import org.apache.jena.query.{ResultSet, ResultSetFactory}
-
-import scala.collection.immutable.Seq
 
 /**
   * Sparql specific akka http circe support.
   *
-  * It uses [[ch.epfl.bluebrain.nexus.commons.sparql.client.RdfMediaTypes.`application/ld+json`]] as the default content
+  * It uses [[ch.epfl.bluebrain.nexus.commons.http.RdfMediaTypes.`application/ld+json`]] as the default content
   * type for encoding json trees into http request payloads.
   */
-trait SparqlCirceSupport extends FailFastCirceSupport {
-
-  override def unmarshallerContentTypes: Seq[ContentTypeRange] =
-    List(`application/json`, RdfMediaTypes.`application/ld+json`)
-
-  final implicit def jsonLdMarshaller(implicit printer: Printer = Printer.noSpaces): ToEntityMarshaller[Json] =
-    Marshaller.withFixedContentType(RdfMediaTypes.`application/ld+json`) { json =>
-      HttpEntity(RdfMediaTypes.`application/ld+json`, printer.pretty(json))
-    }
+trait SparqlCirceSupport extends JsonLdCirceSupport {
 
   final implicit def sparqlResultsUnmarshaller: FromEntityUnmarshaller[ResultSet] =
     Unmarshaller.byteArrayUnmarshaller
