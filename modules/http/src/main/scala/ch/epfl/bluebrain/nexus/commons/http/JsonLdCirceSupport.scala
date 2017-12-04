ea1bd17f1fb6e732aaf700cebdc0e7f4911f5d73
Didac <didac.montero.mendez@gmail.com>
2017-12-04 09:51:04 +0100

    Added remove keys and sortKeys in a common class (#43)

--- modules/http/src/main/scala/ch/epfl/bluebrain/nexus/commons/http/JsonLdCirceSupport.scala
+++ modules/http/src/main/scala/ch/epfl/bluebrain/nexus/commons/http/JsonLdCirceSupport.scala
@@ -5,8 +5,8 @@ import akka.http.scaladsl.model.MediaTypes.`application/json`
 import akka.http.scaladsl.model.{ContentTypeRange, HttpEntity}
 import ch.epfl.bluebrain.nexus.commons.http.JsonLdCirceSupport.OrderedKeys
 import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport
-import io.circe.syntax._
-import io.circe.{Encoder, Json, JsonObject, Printer}
+import io.circe.{Encoder, Json, Printer}
+import ch.epfl.bluebrain.nexus.commons.http.JsonOps._
 
 import scala.collection.immutable.Seq
 
@@ -20,36 +20,6 @@ trait JsonLdCirceSupport extends FailFastCirceSupport {
 
   override def unmarshallerContentTypes: Seq[ContentTypeRange] =
     List(`application/json`, RdfMediaTypes.`application/ld+json`)
-
-  /**
-    * Order json keys.
-    *
-    * @param json the ''json'' we want to order
-    * @param keys the implicitly available definition of the ordering
-    */
-  def sortKeys(json: Json)(implicit keys: OrderedKeys): Json = {
-
-    implicit val _: Ordering[String] = new Ordering[String] {
-      private val middlePos = keys.withPosition("")
-
-      private def position(key: String): Int = keys.withPosition.getOrElse(key, middlePos)
-
-      override def compare(x: String, y: String): Int = {
-        val posX = position(x)
-        val posY = position(y)
-        if (posX == middlePos && posY == middlePos) x compareTo y
-        else posX compareTo posY
-      }
-    }
-
-    def canonicalJson(json: Json): Json =
-      json.arrayOrObject[Json](json, arr => Json.fromValues(arr.map(canonicalJson)), obj => sorted(obj).asJson)
-
-    def sorted(jObj: JsonObject): JsonObject =
-      JsonObject.fromIterable(jObj.toVector.sortBy(_._1).map { case (k, v) => k -> canonicalJson(v) })
-
-    canonicalJson(json)
-  }
 
   /**
     * `A` => HTTP entity
@@ -69,7 +39,7 @@ trait JsonLdCirceSupport extends FailFastCirceSupport {
   final implicit def jsonLdMarshaller(implicit printer: Printer = Printer.noSpaces.copy(dropNullKeys = true),
                                       keys: OrderedKeys = OrderedKeys()): ToEntityMarshaller[Json] =
     Marshaller.withFixedContentType(RdfMediaTypes.`application/ld+json`) { json =>
-      HttpEntity(RdfMediaTypes.`application/ld+json`, printer.pretty(sortKeys(json)))
+      HttpEntity(RdfMediaTypes.`application/ld+json`, printer.pretty(json.sortKeys))
     }
 }
 
@@ -81,7 +51,7 @@ object JsonLdCirceSupport extends JsonLdCirceSupport {
     * @param keys list of strings which defines the ordering for the JSON-LD keys
     */
   final case class OrderedKeys(keys: List[String]) {
-    lazy val withPosition = keys.zipWithIndex.toMap
+    lazy val withPosition: Map[String, Int] = keys.zipWithIndex.toMap
   }
   object OrderedKeys {
 
