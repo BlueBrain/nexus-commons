483922d96644c1c09d87dc343414eadd8ad980bd
Didac Montero <didac.monteromendez@epfl.ch>
2018-01-15 11:18:23 +0100

    Fix test

--- modules/query-types/src/test/scala/ch/epfl/bluebrain/nexus/commons/types/search/SortSpec.scala
+++ modules/query-types/src/test/scala/ch/epfl/bluebrain/nexus/commons/types/search/SortSpec.scala
@@ -6,14 +6,6 @@ import org.scalatest.{Matchers, WordSpecLike}
 class SortSpec extends WordSpecLike with Matchers {
 
   "A Sort" should {
-    "reject when empty" in {
-      Sort("") shouldEqual None
-    }
-
-    "reject when not absolute URI " in {
-      Sort("something") shouldEqual None
-    }
-
     "created correctly " in {
       Sort("createdAtTime") shouldEqual Sort(Asc, s"createdAtTime")
       Sort(s"+createdAtTime") shouldEqual Sort(Asc, s"createdAtTime")
