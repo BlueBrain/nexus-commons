67e6759fb4ad12c78b5a8e7e7fe353ad6358267f
Didac <didac.montero.mendez@gmail.com>
2018-02-23 11:35:51 +0100

    Removed modules not required and dependencies (#90)
    * Added a prefix mapping for nxv (#85)
    
    * Added a prefix mapping for nxv
    
    * Bumped nexus/core/resource context version to 0.3.0
    
    * Add credentials support to the Sparql client (#87)
    
    * Setting new version to 0.7.6
    
    * Setting new version to 0.7.7-SNAPSHOT
    
    * Removed container assertion for qualifiedValueShape (#88)
    
    * Cleaned modules not required and dependencies

--- modules/elastic-client/src/main/scala/ch/epfl/bluebrain/nexus/commons/es/client/ElasticBaseClient.scala
+++ modules/elastic-client/src/main/scala/ch/epfl/bluebrain/nexus/commons/es/client/ElasticBaseClient.scala
@@ -3,9 +3,9 @@ package ch.epfl.bluebrain.nexus.commons.es.client
 import akka.http.scaladsl.model.{HttpRequest, StatusCode}
 import cats.MonadError
 import cats.syntax.flatMap._
+import ch.epfl.bluebrain.nexus.commons.es.client.ElasticBaseClient._
 import ch.epfl.bluebrain.nexus.commons.http.HttpClient.{HttpResponseSyntax, UntypedHttpClient}
 import journal.Logger
-import ch.epfl.bluebrain.nexus.commons.es.client.ElasticBaseClient._
 
 /**
   * ElasticBaseClient provides the common methods and vals used for elastic clients.
@@ -17,11 +17,18 @@ abstract class ElasticBaseClient[F[_]](implicit
                                        F: MonadError[F, Throwable]) {
   private[client] val log = Logger[this.type]
 
+  private[client] def execute(req: HttpRequest, expectedCodes: Set[StatusCode]): F[Unit] =
+    executeWith(req, expectedCodes, None)
+
   private[client] def execute(req: HttpRequest, expectedCodes: Set[StatusCode], intent: => String): F[Unit] =
+    executeWith(req, expectedCodes, Some(intent))
+
+  private def executeWith(req: HttpRequest, expectedCodes: Set[StatusCode], intent: => Option[String]): F[Unit] =
     cl(req).discardOnCodesOr(expectedCodes) { resp =>
       ElasticFailure.fromResponse(resp).flatMap { f =>
-        log.error(
-          s"Unexpected ElasticSearch response for intent '$intent':\nRequest: '${req.method} ${req.uri}'\nStatus: '${resp.status}'\nResponse: '${f.body}'")
+        val _ = intent.map(msg =>
+          log.error(
+            s"Unexpected ElasticSearch response for intent '$msg':\nRequest: '${req.method} ${req.uri}'\nStatus: '${resp.status}'\nResponse: '${f.body}'"))
         F.raiseError(f)
       }
     }
@@ -36,5 +43,4 @@ object ElasticBaseClient {
   private[client] val anyIndexPath      = "_all"
   private[client] val ignoreUnavailable = "ignore_unavailable"
   private[client] val allowNoIndices    = "allow_no_indices"
-
 }
