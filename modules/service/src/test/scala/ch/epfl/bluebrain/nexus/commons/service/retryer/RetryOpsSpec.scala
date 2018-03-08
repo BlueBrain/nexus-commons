4ddbdddf8ccd8c4cbf2d04e40d7708e73532bb99
Henry Genet <henry.genet@epfl.ch>
2018-03-08 10:06:59 +0100

    Increase timeouts in tests

--- modules/service/src/test/scala/ch/epfl/bluebrain/nexus/commons/service/retryer/RetryOpsSpec.scala
+++ modules/service/src/test/scala/ch/epfl/bluebrain/nexus/commons/service/retryer/RetryOpsSpec.scala
@@ -36,25 +36,25 @@ class RetryOpsSpec
 
   "A RetryOps" should {
     "retry a future exponentially when it fails" in new Context {
-      retry(future, 3, Backoff(5 seconds, 0)).failed.futureValue(timeout(Span(7, Seconds))) shouldBe SomeError
+      retry(future, 3, Backoff(5 seconds, 0)).failed.futureValue(timeout(Span(7500, Milliseconds))) shouldBe SomeError
       count.get shouldEqual 4
     }
 
     "retry a future exponentially when it fails capped to 1 second" in new Context {
       private implicit val backoff = Backoff(1 seconds, 0)
-      future.retry(3).failed.futureValue(timeout(Span(3, Seconds))) shouldBe SomeError
+      future.retry(3).failed.futureValue(timeout(Span(3500, Milliseconds))) shouldBe SomeError
       count.get shouldEqual 4
     }
 
     "retry a future linearly when it fails" in new Context {
       retry(future, 4, Linear(5 seconds, 300 millis)).failed
-        .futureValue(timeout(Span(3, Seconds))) shouldBe SomeError
+        .futureValue(timeout(Span(3500, Milliseconds))) shouldBe SomeError
       count.get shouldEqual 5
     }
 
     "retry a future linearly when it fails capped to 200 millis" in new Context {
       retry(future, 5, Linear(200 millis, 200 millis)).failed
-        .futureValue(timeout(Span(1, Seconds))) shouldBe SomeError
+        .futureValue(timeout(Span(1500, Milliseconds))) shouldBe SomeError
       count.get shouldEqual 6
     }
 
