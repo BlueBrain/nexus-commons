b855687bc68ea205b01a16125d6c83f7b4dffeb9
Didac <didac.montero.mendez@gmail.com>
2019-03-19 11:12:48 +0100

    Avoided duplicated on sparql head (#162)

--- modules/sparql-client/src/main/scala/ch/epfl/bluebrain/nexus/commons/sparql/client/SparqlResults.scala
+++ modules/sparql-client/src/main/scala/ch/epfl/bluebrain/nexus/commons/sparql/client/SparqlResults.scala
@@ -45,7 +45,8 @@ object SparqlResults {
       * Creates a new head which is a merge of the provided head and the current head
       * @param that the provided head
       */
-    def ++(that: Head): Head = Head(vars ++ that.vars, link.map(l => that.link.getOrElse(List.empty) ++ l))
+    def ++(that: Head): Head =
+      Head((vars ++ that.vars).distinct, link.map(l => (that.link.getOrElse(List.empty) ++ l).distinct))
   }
 
   /**
