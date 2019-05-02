ac63a5f3f4b76214b7fd292b1e85749abe392ed2
Didac <didac.monteromendez@epfl.ch>
2019-05-02 13:54:05 +0200

    Added more NodeEncoders (#167)

--- modules/core/src/main/scala/ch/epfl/bluebrain/nexus/commons/rdf/instances/package.scala
+++ modules/core/src/main/scala/ch/epfl/bluebrain/nexus/commons/rdf/instances/package.scala
@@ -1,6 +1,6 @@
 package ch.epfl.bluebrain.nexus.commons.rdf
 
-import _root_.akka.http.scaladsl.model.Uri
+import _root_.akka.http.scaladsl.model._
 import ch.epfl.bluebrain.nexus.rdf.encoder.NodeEncoder
 import ch.epfl.bluebrain.nexus.rdf.encoder.NodeEncoder.stringEncoder
 import ch.epfl.bluebrain.nexus.rdf.encoder.NodeEncoderError.IllegalConversion
@@ -11,5 +11,16 @@ package object instances {
   implicit val uriNodeEncoder: NodeEncoder[Uri] = node =>
     stringEncoder(node).flatMap { uri =>
       Try(Uri(uri)).toEither.left.map(err => IllegalConversion(s"Invalid URI '${err.getMessage}'"))
+  }
+
+  implicit val uriPathNodeEncoder: NodeEncoder[Uri.Path] = node =>
+    stringEncoder(node).flatMap { path =>
+      Try(Uri.Path(path)).toEither.left.map(err => IllegalConversion(s"Invalid Path '${err.getMessage}'"))
+
+  }
+
+  implicit val mediaTypeNodeEncoder: NodeEncoder[ContentType] = node =>
+    NodeEncoder.stringEncoder(node).flatMap {
+      ContentType.parse(_).left.map(_ => IllegalConversion("Expected a media type, but found otherwise"))
   }
 }
